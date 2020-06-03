f24a27a3f4503c30b14fc5d824ef6d19afa1b943
Carlos Bruno <cbcarlos08@gmail.com>
2020-06-03 10:23:24 -0300

    -

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,18 @@
-import express from 'express'
+import express, { response } from 'express'
 
 const app = express()
 
 app.get('/users', (req, res) =>{
     console.log('Listagem de usuarios');
     res.json({msg: 'Listagem de usuários'})
+})
 
+app.post('/users', (req, res) => {
+    const user = {
+        name: 'Carlos',
+        email: 'carlos@email.com'
+    }
+    return res.json(user)
 })
 
 app.listen(3333)

f5c0cb145d3bded64e97bdce097452bb75b1cdf0
Carlos Bruno <cbcarlos08@gmail.com>
2020-06-02 22:52:40 -0400

    Api

--- REAME.md
+++ REAME.md
@@ -6,3 +6,17 @@
 # Instalar o tsconfig.json
         
     npx tsc --init
+
+# Quase um ORM
+
+http://knexjs.org/
+
+Obs: Só funcina na versão estável, atualmente é a 12
+
+Executar migrations
+
+    npx knex migrate:latest --knexfile knexfile.ts migrate:latest
+
+# Site de imagens
+
+https://unsplash.com/
--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,12 @@
 import express from 'express'
-
+import cors from 'cors'
+import routes from './routes'
+import path from 'path'
 const app = express()
 
-app.get('/users', (req, res) =>{
-    console.log('Listagem de usuarios');
-    res.json({msg: 'Listagem de usuários'})
-
-})
+app.use( cors() )
+app.use(express.json())
+app.use(routes)
+app.use('/uploads',express.static( path.resolve(__dirname,'..','uploads') ))
 
 app.listen(3333)
